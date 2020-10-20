3c7841aac6cc3d7b84c5c7bfcb454ce775abebd6
rifquabdillah <72897695+rifquabdillah@users.noreply.github.com>
2020-10-20 13:44:15 +0700

    Add files via upload

---
+++ IF110119042Latihan9teks.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanpbo;
+
+/**
+ *
+ * @author Stks
+ */
+public class IF110119042Latihan9teks {
+
+public static void main(String[] args){
+    char c;
+    char tab = '\t';
+    char newline = '\n';
+    c = 'A';
+    System.out.println("Hasil" + newline + "Char :" + tab + "berisi " + c);
+    
+}    
+    
+}
